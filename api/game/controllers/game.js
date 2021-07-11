5ed702d0f7239186e54eabec881ec0c51ed39338
Emerson Acácio Feitosa Santos <emerson_acacio@lccv.ufal.br>
2021-07-11 19:41:47 -0300

    banco populado

---
+++ api/game/controllers/game.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
+ * to customize this controller
+ */
+
+module.exports = {
+  populate: async (ctx) => {
+    console.log("Initializing");
+    await strapi.services.game.popilate();
+    console.log("End");
+  },
+};
